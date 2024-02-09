9810732082683fa380deeac4372ae65298fc4f2b
coderipper <hello@joaquinsoza.dev>
2024-02-10 00:25:27 +0100

    using hardhat vars

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,7 +3,6 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const INFURA_API_KEY = vars.get("INFURA_API_KEY");
 const ANKR_API_KEY = vars.get("ANKR_API_KEY");
-const PRIVATE_KEY = vars.get("PRIVATE_KEY");
 const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
 
 const config: HardhatUserConfig = {
@@ -19,23 +18,18 @@ const config: HardhatUserConfig = {
     },
     polygon_mumbai: {
       url: `https://polygon-mumbai.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [PRIVATE_KEY],
     },
     goerli: {
       url: `https://goerli.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [PRIVATE_KEY],
     },
     sepolia: {
       url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [PRIVATE_KEY],
     },
     avalanche_fuji: {
       url: `https://avalanche-fuji.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [PRIVATE_KEY],
     },
     bsc_testnet: {
       url: `https://rpc.ankr.com/bsc_testnet_chapel/${ANKR_API_KEY}`,
-      accounts: [PRIVATE_KEY],
     },
   },
   etherscan: {
